887aaad19a6648d38394484894646e23448f940f
alstj2384 <ass91160@naver.com>
2024-05-14 19:19:10 +0900

    init

---
+++ src/main/java/csu/RouteGuideBackend/RouteGuideBackendApplication.java
@@ -0,0 +1,13 @@
+package csu.RouteGuideBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouteGuideBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouteGuideBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/csu/RouteGuideBackend/RouteGuideBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package csu.RouteGuideBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouteGuideBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
