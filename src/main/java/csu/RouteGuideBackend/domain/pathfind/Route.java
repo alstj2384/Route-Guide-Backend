dfaf169d4ff0dbe760bfda94f079ffd29dd8d8f4
alstj2384 <ass91160@naver.com>
2024-05-16 14:22:26 +0900

    [Feat : Pathfind, Route Entity 추가]

---
+++ src/main/java/csu/RouteGuideBackend/domain/pathfind/Route.java
@@ -0,0 +1,40 @@
+package csu.RouteGuideBackend.domain.pathfind;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Route {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pathfind_id", nullable = false)
+    private Pathfind pathfind;
+
+    @Column(name = "x", nullable = false)
+    private double x;
+    @Column(name = "y", nullable = false)
+    private double y;
+    @Column(name = "description", nullable = false)
+    private String description;
+    @Column(name = "index", nullable = false)
+    private int index;
+
+    @Override
+    public String toString() {
+        return "Route{" +
+                "id=" + id +
+                ", x=" + x +
+                ", y=" + y +
+                ", description='" + description + '\'' +
+                ", index=" + index +
+                '}';
+    }
+}
