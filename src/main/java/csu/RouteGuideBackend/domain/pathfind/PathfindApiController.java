db814b4cc3f5a2e24b57c858329fa41ec706b52e
alstj2384 <ass91160@naver.com>
2024-05-16 14:57:21 +0900

    [Feat : PathfindApiController 추가]

---
+++ src/main/java/csu/RouteGuideBackend/domain/pathfind/PathfindApiController.java
@@ -0,0 +1,51 @@
+package csu.RouteGuideBackend.domain.pathfind;
+
+import csu.RouteGuideBackend.config.PrincipalDetails;
+import csu.RouteGuideBackend.dto.DestinationViewDto;
+import csu.RouteGuideBackend.dto.StartPathFindDto;
+import csu.RouteGuideBackend.dto.StartPathFindViewDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.http.HttpResponse;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/path-find")
+@Slf4j
+public class PathfindApiController {
+
+    private final PathfindService pathfindService;
+
+    @GetMapping("/search")
+    public ResponseEntity<List<DestinationViewDto>> SearchDestination(@RequestParam("destination") String destination,
+                                                                      @RequestParam("x") double x, @RequestParam("y") double y) throws Exception{
+        log.info("목적지 검색");
+        HttpResponse<String> response = pathfindService.searchDestination(destination, x, y);
+        List<DestinationViewDto> destinationViewDtos = pathfindService.parseDestination(response.body()).orElseThrow(() -> new IllegalArgumentException("검색결과가 존재하지 않습니다!"));
+        return ResponseEntity.ok().body(destinationViewDtos);
+    }
+
+    @PostMapping("/start")
+    public ResponseEntity<StartPathFindViewDto> startPathFind(@AuthenticationPrincipal PrincipalDetails principalDetails, @RequestBody StartPathFindDto dto) throws Exception{
+        log.info("길찾기 시작 요청");
+        HttpResponse<String> response = pathfindService.startPathfind(dto);
+        StartPathFindViewDto startPathFindViewDto = pathfindService.parsePath(principalDetails.getUsername(), response.body());
+        if(startPathFindViewDto == null){
+            throw new Exception("파싱중 오류 발생");
+        }
+
+//        objectMapper.writeValueAsString(startPathFindViewDto);
+        return ResponseEntity.ok().body(startPathFindViewDto);
+    }
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> IlliegalArgumentException(IllegalArgumentException ex) {
+        // 예외 처리 로직
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
