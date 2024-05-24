ae72b081fdf74ba651809f804b9c3af870050438
alstj2384 <ass91160@naver.com>
2024-05-24 09:06:21 +0900

    [Location domain 마무리]

--- src/main/java/csu/RouteGuideBackend/domain/location/LocationService.java
+++ src/main/java/csu/RouteGuideBackend/domain/location/LocationService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Date;
@@ -19,6 +20,7 @@ public class LocationService {
     private final MemberRepository memberRepository;
 
 
+    @Transactional
     public void update(int id, double lat, double lon){
         Member find = memberRepository.findById(id).orElseThrow(() -> new UsernameNotFoundException("회원 조회 실패"));
 
@@ -31,15 +33,11 @@ public class LocationService {
                     .build();
             Location save = locationRepository.save(now);
             find.setLocation(save);
-            memberRepository.save(find);
         } else {
             Location now = find.getLocation();
             now.setUpdatedAt(LocalDateTime.now());
             now.setLat(lat);
             now.setLon(lon);
-            Location save = locationRepository.save(now);
-            find.setLocation(save);
-            memberRepository.save(find);
         }
 
 
