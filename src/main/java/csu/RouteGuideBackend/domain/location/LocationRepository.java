e8ddb2ffa1268def846715406452723ed5d95fdf
alstj2384 <ass91160@naver.com>
2024-06-06 23:12:54 +0900

    [Feat : location 삭제]

--- src/main/java/csu/RouteGuideBackend/domain/location/LocationRepository.java
+++
@@ -1,8 +0,0 @@
-package csu.RouteGuideBackend.domain.location;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LocationRepository extends JpaRepository<Location, Long> {
-}
